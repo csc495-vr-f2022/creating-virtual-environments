2ea1bf664457ffdd912c93315a4a66e06df018b4
Regis Kopper <kopper@uncg.edu>
2022-09-06 12:05:17 -0400

    Added dragonite and deleted mustang.

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,23 @@
  * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  */ 
 
-import { Color3, float, HemisphericLight, MeshAssetTask, SceneLoader, SpotLight, StandardMaterial, Texture, UniversalCamera, Vector3 } from "@babylonjs/core";
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { Scene } from "@babylonjs/core/scene";
-import "@babylonjs/loaders/glTF";
+import { Vector3, Color3 } from "@babylonjs/core/Maths/math";
+import { UniversalCamera } from "@babylonjs/core/Cameras/universalCamera";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
+import { StandardMaterial } from "@babylonjs/core";
 
+// loaders
+//import "@babylonjs/core/Loading/Plugins/babylonFileLoader";
+import "@babylonjs/loaders/OBJ/objFileLoader";
+import "@babylonjs/loaders/glTF/2.0/glTFLoader";
 
 // Required to populate the Create methods on the mesh class. 
 // Without this, the bundle would be smaller,
 // but the createXXX methods from mesh would not be accessible.
-import {MeshBuilder} from  "@babylonjs/core/Meshes/meshBuilder";
-import "@babylonjs/core/Materials/standardMaterial"
+//import {MeshBuilder} from  "@babylonjs/core/Meshes/meshBuilder";
 
 // Import debug layer
 import "@babylonjs/core/Debug/debugLayer";
@@ -47,35 +53,23 @@ class Game
         // Default intensity is 1. Let's dim the light a small amount
         light.intensity = 1.0;
 
-        SceneLoader.ImportMesh("","assets/models/","mustang.babylon", scene);
-
+        
+        var count = 0;
         SceneLoader.ImportMesh("", "assets/models/", "dragonite.obj", scene, (meshes) => {
-            meshes[0].name = "Dragonite";
+            meshes[0].name = "dragonite";
+            meshes[0].scaling = new Vector3(10, 10, 10);
+            meshes[0].rotation = new Vector3(0, Math.PI, 0);
+        });
+
+        var world = SceneLoader.ImportMesh("", "assets/models/", "world.glb", scene, (meshes) => {
+            meshes[0].name = "world";
             meshes[0].position = new Vector3(-75, -22, -50);
 
             meshes.forEach((mesh) => {
-                console.log("Loaded mesh: " + mesh.name)
+                console.log("Loading mesh: " + mesh.name);
             })
         });
 
-        // // Our built-in 'sphere' shape.
-        // var sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-
-        // // Move the sphere upward 1/2 its height
-        // sphere.position.y = 1;
-
-        // // Our built-in 'ground' shape.
-        var ground = MeshBuilder.CreateGround("ground", {width: 100, height: 100}, scene);
-
-        // // Import a grid texture and set it to be tiled
-        // var gridTexture = new Texture("textures/grid.png", scene);
-        // gridTexture.uScale = 100;
-        // gridTexture.vScale = 100;
-
-        // // Create a ground material and assign it to the ground object
-        // var groundMaterial = new StandardMaterial("groundMaterial", scene);
-        // groundMaterial.diffuseTexture = gridTexture;
-        // ground.material = groundMaterial;
 
         // Show the debug scene explorer and object inspector
         // You should comment this out when you build your final program 
